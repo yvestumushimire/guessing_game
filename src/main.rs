30d66484ade2348e958ba6084099f35b09cb86fb
Tumushimire Yves <yvestumushimire@gmail.com>
2022-04-16 21:09:30 +0200

    added multiple tries

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,26 @@ use std::io;
 fn main() {
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1..101);
-    println!("Please input your guess.");
-    let mut guess = String::new(); // mutable string
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
+    let mut tries = 0;
+    loop {
+        println!("Please input your guess.");
+        let mut guess = String::new(); // mutable string
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse()
-        .expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
     
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        tries += 1;
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!, Tries: {}", tries);
+                break;
+            }
+        }
     }
-    println!("You guessed: {} correct number: {}", guess, secret_number);
 }
